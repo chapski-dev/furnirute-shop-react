a59411dfb40d28f9d60f0fcf78316ee5d756da00
Chapski <alexey.chapski@gmail.com>
2021-10-15 15:47:00 +0300

    added icons on section2

--- src/components/sections/Section2/Section2Item.jsx
+++ src/components/sections/Section2/Section2Item.jsx
@@ -1,3 +1,6 @@
+import { Avatar } from "antd";
+import { FireOutlined } from '@ant-design/icons';
+
 import "./Section2Item.scss"
 
 
@@ -8,7 +11,7 @@ export const Section2Item = (props) => {
   return (
     <div className="section2-item">
           <div className="section2-item__img">
-            <i className={`icon-${props.iconName}`}></i>
+            <FireOutlined  style={{ fontSize: '36px', color: '#08c', margin:'0 24px 0 0' }}/>
           </div>
           <div className="section2-item-info">
             <div className="section2-item__title">{props.title}</div>
